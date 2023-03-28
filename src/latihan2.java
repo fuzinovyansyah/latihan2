ee4793d1c5d1cfdfa0e5de36aeaecd0241c8fdef
fuzinovyansyah <116516359+fuzinovyansyah@users.noreply.github.com>
2023-03-28 08:58:49 +0700

    Update latihan2.java

--- src/latihan2.java
+++ src/latihan2.java
@@ -2,10 +2,10 @@ import java.util.LinkedList;
 
 public class latihan2 {
     public static void main(String[] args) {
-        // Membuat objek Hewan pada Linkedlist
+        
         LinkedList<String> hewan = new LinkedList<>();
 
-        // Menambahkan elemen ke Linkedlist
+        
         hewan.add("Sapi");
         hewan.add("Kelinci");
         hewan.add("Kambing");
@@ -15,7 +15,7 @@ public class latihan2 {
 
         System.out.println("Nama Hewan: " + hewan);
         System.out.println("Hewan yang di hapus : Kelinci, Kambing");
-        //hapus index
+        
         LinkedList<String> deleteHewan = new LinkedList<>();
         hewan.remove("Kelinci");
         hewan.remove("Kambing");
